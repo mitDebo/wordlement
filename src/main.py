0b77a494c99a19b03916fa435b4c49af64946eb9
Kelly Weaver <kel.weaver@gmail.com>
2022-01-29 13:48:08 -0500

    Now I did write code to pick on Josh

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import discord
+from discord import Message, Member
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 import os
 
 discord_token = os.environ.get("DISCORD_TOKEN")
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 slash = SlashCommand(client, sync_commands=True)
+
 
 
 @client.event
 async def on_ready():
     print('Ready!')
+
+@client.event
+async def on_message(message:Message):
+    if message.author == bot.user:
+        return
+
+    if is_josh(message.author):
+        message.reply("Fuck you, Josh")
 
 
 @slash.slash(
@@ -21,5 +31,9 @@ async def on_ready():
 async def _test(ctx: SlashContext):
     await ctx.send(content="Hello World!")
 
+def is_josh(member:Member) -> bool:
+    josh_id = 273219725587382272  # josh's discord id that I looked up
+    return member.id == josh_id
 
-client.run(discord_token)
+
+bot.run(discord_token)
