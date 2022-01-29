ccf9d31ae0bcfe00011e94b955e9fab559e4ab75
Kelly Weaver <kel.weaver@gmail.com>
2022-01-29 13:21:07 -0500

    Initial setup and getting hello world working for the bot

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print("Hello World")
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+import os
+
+discord_token = os.environ.get("DISCORD_TOKEN")
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
+slash = SlashCommand(client, sync_commands=True)
+
+
+@client.event
+async def on_ready():
+    print('Ready!')
+
+
+@slash.slash(
+    name="hello",
+    description="This is just a test command, nothing more."
+)
+async def _test(ctx: SlashContext):
+    await ctx.send(content="Hello World!")
+
+
+client.run(discord_token)
