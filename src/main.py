7670c29cbf3d764f0bf6294f5ecb7f33e89fdff1
Kelly Weaver <kel.weaver@gmail.com>
2022-01-29 15:53:22 -0500

    Removed Josh trolling stuff

--- src/main.py
+++ src/main.py
@@ -20,21 +20,13 @@ async def on_message(message:Message):
     if message.author == bot.user:
         return
 
-    if is_josh(message.author):
-        message.reply("Fuck you, Josh")
-
-
+    
 @slash.slash(
     name="hello",
     description="This is just a test command, nothing more."
 )
 async def _test(ctx: SlashContext):
     await ctx.send(content="Hello World!")
-
-
-def is_josh(member: Member) -> bool:
-    josh_id = 273219725587382272  # josh's discord id that I looked up
-    return member.id == josh_id
 
 
 bot.run(discord_token)
