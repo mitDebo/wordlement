28bd29c5e2f5aafa319b689511b4e7cea8879225
Kelly Weaver <kel.weaver@gmail.com>
2022-01-30 10:39:35 -0500

    Name change; message parsing implemented, checking for wordle scores

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
-import discord
 import os
-import wc_admin
-from discord import Message, TextChannel
+import re
+from discord import Message, Intents
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
-from discord_slash.utils.manage_commands import create_option
-
 discord_token = os.environ.get("DISCORD_TOKEN")
-intents = discord.Intents.all()
+intents = Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
 slash = SlashCommand(bot, sync_commands=True)
 
@@ -19,48 +16,58 @@ async def on_ready():
 
 
 @slash.slash(
-    name="wcadmin",
-    description="Wordicomp administration CLI",
-    guild_ids=[699613886487461918],
-    options=[
-        create_option(name="watch_channel",
-                      description="Sets the channel the bot watches. If not set, defaults to all channels",
-                      required=False,
-                      option_type=7),
-        create_option(name="output_channel",
-                      description="Sets the channel the bot writes results. If not set, defaults to system channel",
-                      required=False,
-                      option_type=7)
-    ]
+    name="hello",
+    description="hello",
+    guild_ids=[937340367110553691]
 )
-async def _update_admin(ctx: SlashContext,
-                        watch_channel: TextChannel = None,
-                        output_channel: TextChannel = None):
-    member = ctx.author
-    if member == bot.user:
-        return
-
-    if not member.top_role.permissions.administrator:
-        await ctx.send("Permission denied: not an admin")
-        return
-
-    if isinstance(watch_channel, TextChannel):
-        success = wc_admin.update_watch_channel(watch_channel)
-        reply = "Watch channel updated to #" + watch_channel.name if success else "There was an error updating the " \
-                                                                                  "watch channel "
-        await ctx.send(reply)
-
-    if isinstance(output_channel, TextChannel):
-        success = wc_admin.update_output_channel(output_channel)
-        reply = "Results channel updated to #" + output_channel.name if success else "There was an error updating the " \
-                                                                                     "output channel "
-        await ctx.send(reply)
+async def _hello_world(ctx: SlashContext):
+    await ctx.send("Hello, world")
 
 
 @bot.event
 async def on_message(message: Message):
-    if message.author == bot.user:
+    if message.author.bot:
         return
+
+    match = re.match(r"Wordle ([0-9]+) ([1-6|X])/6(\*)?", message.content)
+    if match is not None:
+        player = message.author
+        wordle_id = match.group(1).strip()
+        score = match.group(2).strip()
+        is_hard = True if match.group(3) is not None else False
+
+        if score == "X":
+            score = "7"
+        score = int(score)
+
+        await message.channel.send(f"{golf_score(score)} Score recorded for <@{message.author.id}>"
+                                   f"on {wordle_game(wordle_id, is_hard)}")
+
+
+def golf_score(score: int) -> str:
+    if score == 1:
+        return "HOLE IN ONE!!"
+    if score == 2:
+        return "Eagle!"
+    if score == 3:
+        return "Birdie!"
+    if score == 4:
+        return "Par."
+    if score == 5:
+        return "Bogey."
+    if score == 6:
+        return "Double Bogey."
+    if score == 7:
+        return "Womp womp..."
+
+
+def wordle_game(id: str, is_hard: bool) -> str:
+    game_str = f"Wordle {id}"
+    if is_hard:
+        game_str = game_str + " (Hard Mode)"
+
+    return game_str
+
 
 
 bot.run(discord_token)
